91a977029ff42ad25929fc960d7a97d8a84b493b
JayaKrishanS <129932233+JayaKrishanS@users.noreply.github.com>
2024-07-17 13:24:06 +0530

    Add files via upload

---
+++ zoho_text_SQL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE User_list(
+User_name VARCHAR(100),
+passwords VARCHAR(100),
+Age INT,
+DOY INT,
+contact_no INT,
+mail_id VARCHAR(100)
+)
+
+SELECT * FROM user_list;
+
+INSERT into user_list VALUES('xyz1999', 'xyz@passwords', 24, 1999, 1234567890, 'xyz@gmail.com');
